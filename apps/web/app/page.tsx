7fc3e8dd9ae8a7107f775e8d654570a87d465497
c3k90 <c3k90@icloud.com>
2025-12-30 20:52:53 +0000

    Fix docker build file

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,4 @@
 import GameDashboard from "@/components/game-dashboard";
-import { Header } from "@/components/header";
-import { Button, ThemeToggle } from "@repo/ui";
 
 export default function Home() {
   return <GameDashboard />;
